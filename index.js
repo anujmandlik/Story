80c0b7b4b98b0d2a32f052b068099d96d98bc480
unknown <anujmandlik7562@gmail.com>
2024-06-10 16:57:54 +0530

    Second commit all files

---
+++ index.js
@@ -0,0 +1,50 @@
+// let a = 30;
+// let b = 10;
+// console.log("++a = ", ++a)
+// console.log("a++ = ", a++)
+// console.log("--a= ", --a)
+// console.log("a-- = ", a--)
+// console.log("a = ", a)
+// console.log("a-- = ", a--)
+
+// let abc= "anuj"
+// console.log(abc)
+
+// let today = new Date();
+// // console.log("The today date is ", today);
+
+// console.log("today date is ", today.getDate());
+// console.log("today day is ", today.getMonth());
+// console.log("this year is ", today.getFullYear());
+
+
+// console.log(today.getDate(),"/",today.getMonth()+1,"/",today.getFullYear());
+
+// //how to convert number into string
+
+// console.log("Todays date and time in string :", today.toLocaleString());
+// console.log("Todays date in string :", today.toDateString());
+
+//object
+
+// let theObject = {
+//     id: 12345,
+//     name: "Anuj",
+//     age: 24,
+// }
+
+// console.log("The object: ", theObject);
+
+// console.log("The Object Name is: ", theObject.name);
+// console.log("The Object Age is: ", theObject['age']);
+
+// //To add something in object
+// theObject.phone = 7276564237;
+// console.log("The Object phone is :", theObject);
+
+// // To delete something from object
+// delete theObject.age;
+// console.log("The Object phone is :", theObject);
+
+// Regex - Regular Expression
+
